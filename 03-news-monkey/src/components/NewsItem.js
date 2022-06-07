e7d47c49f4965d68ce9551667dbef3b422911070
Udit8158 <kunduudit8158@gmail.com>
2022-06-07 23:39:04 +0530

    Testing firstinter val with sampel news is done successfully.

--- 03-news-monkey/src/components/NewsItem.js
+++ 03-news-monkey/src/components/NewsItem.js
@@ -3,15 +3,20 @@ import React, { Component } from "react";
 export class NewsItem extends Component {
   render() {
     // Use props in newsitem...
-    let { title, description, url } = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <>
         <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={`${url}`} className="btn btn-primary btn-sm">
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-primary btn-sm"
+              rel="noopener noreferrer" // warning here so solve by this with the help of google
+            >
               Go somewhere
             </a>
           </div>
