b13a501ac7c50b501f62f3a8075704a6a02cc7bb
Udit8158 <kunduudit8158@gmail.com>
2022-06-07 18:15:02 +0530

    basic file and bootstrap and component setup for newsmonkey.

---
+++ 03-news-monkey/src/components/News.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class News extends Component {
+  render() {
+    return <div>This is news component.</div>;
+  }
+}
+
+export default News;
---
+++ 03-news-monkey/src/components/NewsItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    return <div>This is a news item component.</div>;
+  }
+}
+
+export default NewsItem;
