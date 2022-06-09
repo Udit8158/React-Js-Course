0bd3950c9a460fccd9b7295c250d99d0682df97b
Udit8158 <kunduudit8158@gmail.com>
2022-06-09 19:33:46 +0530

    solve the issue of navbar in mobile.

--- 03-news-monkey/src/components/Navbar.js
+++ 03-news-monkey/src/components/Navbar.js
@@ -12,6 +12,18 @@ export class Navbar extends Component {
               Navbar
             </Link>
 
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
             <div
               className="collapse navbar-collapse"
               id="navbarSupportedContent"
