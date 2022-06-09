bf9d5196264d5dd48c6800177357a573019a6b0e
Udit8158 <kunduudit8158@gmail.com>
2022-06-09 08:35:17 +0530

    fixing bugs on next prage and prev page btns.

--- 03-news-monkey/src/components/News.js
+++ 03-news-monkey/src/components/News.js
@@ -9,12 +9,14 @@ export class News extends Component {
       articles: [],
       page: 1,
       totalResults: 0,
+      pageSize: 20,
+      maxPages: 0,
     };
   }
   // Use in componentDidMount (don't know why... this is actually use of life cycale.)
   async componentDidMount() {
     // Fetching from new api...
-    const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${this.state.page}&apiKey=d34ab57786dc4ece9656435b68646fa5`;
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${this.state.page}&${this.state.pageSize}&apiKey=d34ab57786dc4ece9656435b68646fa5`;
     const data = await fetch(url);
     const parsedData = await data.json();
     // console.log(parsedData);
@@ -22,6 +24,7 @@ export class News extends Component {
 
     this.setState({
       totalResults: parsedData.totalResults,
+      maxPages: Math.ceil(parsedData.totalResults / this.state.pageSize),
       articles: parsedData.articles.filter(
         (e) =>
           e.title !== null &&
@@ -36,53 +39,53 @@ export class News extends Component {
   // Prev and next page logic
 
   previousPage = async () => {
-    if (this.state.page > 1) {
-      const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${
-        this.state.page - 1
-      }&apiKey=d34ab57786dc4ece9656435b68646fa5`;
-      const data = await fetch(url);
-      const parsedData = await data.json();
-      // console.log(parsedData);
-      // changing state
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${
+      this.state.page - 1
+    }&${this.state.pageSize}&apiKey=d34ab57786dc4ece9656435b68646fa5`;
+    console.log(url);
+    const data = await fetch(url);
+    const parsedData = await data.json();
+    // console.log(parsedData);
+    // changing state
 
-      this.setState({
-        page: this.state.page - 1,
-        totalResults: parsedData.totalResults,
-        articles: parsedData.articles.filter(
-          (e) =>
-            e.title !== null &&
-            e.description !== null &&
-            e.urlToImage !== null &&
-            e.url !== null
-        ),
-      });
-    }
+    this.setState({
+      page: this.state.page - 1,
+      totalResults: parsedData.totalResults,
+      articles: parsedData.articles.filter(
+        (e) =>
+          e.title !== null &&
+          e.description !== null &&
+          e.urlToImage !== null &&
+          e.url !== null
+      ),
+    });
   };
 
   nextPage = async () => {
     // Page counting logic
-    const pageSize = 20; // resuls for 1 page (It might less than 20 because in filter they will gone for null.)
-    const maxPages = Math.ceil(this.state.totalResults / pageSize);
-    if (this.state.page < maxPages) {
-      const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${
-        this.state.page + 1
-      }&apiKey=d34ab57786dc4ece9656435b68646fa5`;
-      const data = await fetch(url);
-      const parsedData = await data.json();
-      // console.log(parsedData);
-      // changing state
-      this.setState({
-        page: this.state.page + 1,
-        articles: parsedData.articles.filter(
-          (e) =>
-            e.title !== null &&
-            e.description !== null &&
-            e.urlToImage !== null &&
-            e.url !== null
-        ),
-      });
-      // console.log("2", url);
-    }
+    // const pageSize = this.state.pageSize; // resuls for 1 page (It might less than 20 because in filter they will gone for null.)
+    const maxPages = this.state.maxPages;
+
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${
+      this.state.page + 1
+    }&${this.state.pageSize}&apiKey=d34ab57786dc4ece9656435b68646fa5`;
+    console.log(url);
+
+    const data = await fetch(url);
+    const parsedData = await data.json();
+    // console.log(parsedData);
+    // changing state
+    this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles.filter(
+        (e) =>
+          e.title !== null &&
+          e.description !== null &&
+          e.urlToImage !== null &&
+          e.url !== null
+      ),
+    });
+    // console.log("2", url);
   };
 
   render() {
@@ -122,7 +125,7 @@ export class News extends Component {
               type="button"
               className="btn btn-primary"
               onClick={this.nextPage}
-              disabled={this.state.page === this.maxPages ? true : false}
+              disabled={this.state.page === this.state.maxPages ? true : false}
             >
               Next &rarr;
             </button>
