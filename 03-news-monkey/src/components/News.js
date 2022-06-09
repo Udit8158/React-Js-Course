ce4b1d49c30b6e0366dfc5e1d8746fa0457239d3
Udit8158 <kunduudit8158@gmail.com>
2022-06-09 09:07:44 +0530

    successfully add spinner and fix more bugs.

--- 03-news-monkey/src/components/News.js
+++ 03-news-monkey/src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner.svg";
 
 export class News extends Component {
   // Setting state...
@@ -11,12 +12,14 @@ export class News extends Component {
       totalResults: 0,
       pageSize: 20,
       maxPages: 0,
+      loading: false,
     };
   }
   // Use in componentDidMount (don't know why... this is actually use of life cycale.)
   async componentDidMount() {
+    this.setState({ loading: true });
     // Fetching from new api...
-    const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${this.state.page}&${this.state.pageSize}&apiKey=d34ab57786dc4ece9656435b68646fa5`;
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${this.state.page}&pageSize=${this.props.pageSize}&apiKey=d34ab57786dc4ece9656435b68646fa5`;
     const data = await fetch(url);
     const parsedData = await data.json();
     // console.log(parsedData);
@@ -24,7 +27,7 @@ export class News extends Component {
 
     this.setState({
       totalResults: parsedData.totalResults,
-      maxPages: Math.ceil(parsedData.totalResults / this.state.pageSize),
+      maxPages: Math.ceil(parsedData.totalResults / this.props.pageSize),
       articles: parsedData.articles.filter(
         (e) =>
           e.title !== null &&
@@ -32,6 +35,7 @@ export class News extends Component {
           e.urlToImage !== null &&
           e.url !== null
       ),
+      loading: false,
     });
     // console.log("1", url);
   }
@@ -39,9 +43,10 @@ export class News extends Component {
   // Prev and next page logic
 
   previousPage = async () => {
+    this.setState({ loading: true });
     const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${
       this.state.page - 1
-    }&${this.state.pageSize}&apiKey=d34ab57786dc4ece9656435b68646fa5`;
+    }&pageSize=${this.props.pageSize}&apiKey=d34ab57786dc4ece9656435b68646fa5`;
     console.log(url);
     const data = await fetch(url);
     const parsedData = await data.json();
@@ -58,17 +63,19 @@ export class News extends Component {
           e.urlToImage !== null &&
           e.url !== null
       ),
+      loading: false,
     });
   };
 
   nextPage = async () => {
+    this.setState({ loading: true });
     // Page counting logic
     // const pageSize = this.state.pageSize; // resuls for 1 page (It might less than 20 because in filter they will gone for null.)
-    const maxPages = this.state.maxPages;
+    // const maxPages = this.state.maxPages;
 
     const url = `https://newsapi.org/v2/top-headlines?country=in&category=business&page=${
       this.state.page + 1
-    }&${this.state.pageSize}&apiKey=d34ab57786dc4ece9656435b68646fa5`;
+    }&pageSize=${this.props.pageSize}&apiKey=d34ab57786dc4ece9656435b68646fa5`;
     console.log(url);
 
     const data = await fetch(url);
@@ -84,33 +91,43 @@ export class News extends Component {
           e.urlToImage !== null &&
           e.url !== null
       ),
+      loading: false,
     });
     // console.log("2", url);
   };
 
   render() {
+    let { pageSize } = this.props;
     // console.log(this.state.articles);
     return (
       <>
         <div className="container">
-          <h1 className="">Our top headlines</h1>
-          <div className="row my-5">
-            {/* Generate this part */}
-            {/* // In ract for each is not working properly we should use map to itreate array.  */}
-            {this.state.articles.map((e) => {
-              return (
-                <div className="col-md-3 my-3" key={e.url}>
-                  <NewsItem
-                    title={e.title.slice(0, 38) + "...."}
-                    description={e.description.slice(0, 83) + "...."}
-                    imageUrl={e.urlToImage}
-                    newsUrl={e.url}
-                  />
-                </div>
-              );
-            })}
-          </div>
+          <h1 className="text-center">Our top headlines</h1>
 
+          {/* Means  if this.state.loading === true then only display this loadig spinner  (very useful syntax) */}
+          {this.state.loading && (
+            <div className="text-center">
+              <img src={Spinner} alt="React Logo" />
+            </div>
+          )}
+          {!this.state.loading && (
+            <div className="row my-5">
+              {/* Generate this part */}
+              {/* // In ract for each is not working properly we should use map to itreate array.  */}
+              {this.state.articles.map((e) => {
+                return (
+                  <div className="col-md-3 my-3" key={e.url}>
+                    <NewsItem
+                      title={e.title.slice(0, 38) + "...."}
+                      description={e.description.slice(0, 83) + "...."}
+                      imageUrl={e.urlToImage}
+                      newsUrl={e.url}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          )}
           {/* Previous and next btn container */}
           <div className="container my-3 d-flex justify-content-between">
             <button
